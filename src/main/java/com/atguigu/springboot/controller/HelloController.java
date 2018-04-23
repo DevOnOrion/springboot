3e128028ddbf49c193623bf25e59c217769068aa
DevOnOrion <tangj2017@163.com>
2018-04-23 11:20:48 +0800

    更新HelloController类和success.html文件

--- src/main/java/com/atguigu/springboot/controller/HelloController.java
+++ src/main/java/com/atguigu/springboot/controller/HelloController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Arrays;
 import java.util.Map;
 
 /**
@@ -25,7 +26,8 @@ public class HelloController {
     //查出一些数据在页面展示
     @RequestMapping("/success")
     public String success(Map<String, java.lang.Object> map){
-        map.put("hello","你好");
+        map.put("hello","<h1>你好</h1>");
+        map.put("users", Arrays.asList("zhangsan","lisi","wangwu"));
         return "success";
     }
 }
