6add29afcebfd330eca9847e2b8e177a6be43125
DevOnOrion <tangj2017@163.com>
2018-04-23 23:30:45 +0800

    SpringBoot MVC

---
+++ src/main/java/com/atguigu/springboot/config/MyMvcConfig.java
@@ -0,0 +1,36 @@
+package com.atguigu.springboot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author tangjun
+ * @Description
+ * @date 2018/4/23/02315:32
+ */
+//使用WebMVCConfigurerAdapter可以扩展springmvc的功能
+//@EnableWebMvc
+@Configuration
+public class MyMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        //浏览器发送 /atguigu 请求来到 success
+        registry.addViewController("/atguigu").setViewName("success");
+    }
+
+    //所有的WebMvcConfigurerAdapter组件都会一起起作用
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer(){
+        WebMvcConfigurer adapter = new WebMvcConfigurer() {
+            @Override
+            public void addViewControllers(ViewControllerRegistry registry) {
+                registry.addViewController("/").setViewName("login");
+                registry.addViewController("/index.html").setViewName("login");
+            }
+        };
+        return adapter;
+    }
+}
